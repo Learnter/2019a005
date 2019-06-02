3c724de59385811ee6816ccd2b5eb7225d34581f
Learnter <17818556509@163.com>
2019-06-02 22:58:00 +0800

    修改搜索页面

--- pages/search/search.vue
+++ pages/search/search.vue
@@ -1,16 +1,59 @@
 <template>
-	<view class="search">
-    <view class="historyBox">
-      <h3>搜索发现</h3>
-      <view class="history-tab">
-        
+	<!--搜索页面 -->
+	<view class="searchPage">
+		
+		<view class="headBox">
+			<!-- 首页头部 -->
+			<image class="bgImage" src="../../static/ga005_26_1.png" mode="widthFix"></image>
+			<view class="contentBox">
+				<view class="con_text">
+					<h2>OPAY</h2>
+					<view class="con_buttom">
+		        <uni-icon type="arrowleft" size="30" color="white"></uni-icon>
+						<view class="conImg">
+							<image src="../../static/2019_a005_27.png" mode="widthFix"></image>
+						</view>
+		        <text>搜索</text>
+					</view>
+				</view>
+			</view>
+		</view>
+		
+		<view class="search-main">
+    <view class="search-history">
+      <h2>搜索发现</h2>
+      <view class="history-items">
+        	<view class="tag-view">
+        		 <uni-tag :inverted="true" :circle="true" text="标签" />
+        	</view>
+					<view class="tag-view">
+						 <uni-tag :inverted="true" :circle="true" text="我的标签标签" />
+					</view>
+					<view class="tag-view">
+						 <uni-tag :inverted="true" :circle="true" text="历史1的标签" />
+					</view>
+					<view class="tag-view">
+						 <uni-tag :inverted="true" :circle="true" text="历史2的标签" />
+					</view>
+					<view class="tag-view">
+						 <uni-tag :inverted="true" :circle="true" text="历史4的标签" />
+					</view>
+					<view class="tag-view">
+						 <uni-tag :inverted="true" :circle="true" text="历史3的标签" />
+					</view>
+					<view class="tag-view">
+						 <uni-tag :inverted="true" :circle="true" text="历史3的标签" />
+					</view>
       </view>
     </view>
+
     <view class="referrals">
-      <h3>好货推介</h3>
+      <h2>好货推介</h2>
       <view class="category-list">
          <view class="category-li" @tap="toDetail">
-           <image src="../../static/ga005_40.png" mode="widthFix" ></image>
+          <view class="category-li-image">
+          	<image src="../../static/ga005_42.png" mode="aspectFit"></image>
+          </view>
            <view class="cate-li-right">
              <view class="li-r-top">
                <view class="li-r-top-left">
@@ -30,7 +73,9 @@
            </view>
          </view>
          <view class="category-li">
-           <image src="../../static/ga005_40.png" mode="widthFix" style="width:30%"></image>
+         <view class="category-li-image">
+         	 <image src="../../static/ga005_40.png" mode="aspectFit"></image>
+         </view>
            <view class="cate-li-right">
              <view class="li-r-top">
                <view class="li-r-top-left">
@@ -50,7 +95,9 @@
            </view>
          </view>
          <view class="category-li">
-           <image src="../../static/ga005_40.png" mode="widthFix" style="width:30%"></image>
+					 <view class="category-li-image">
+					 	<image src="../../static/ga005_41.png" mode="aspectFit"></image>
+					 </view>
            <view class="cate-li-right">
              <view class="li-r-top">
                <view class="li-r-top-left">
@@ -70,7 +117,9 @@
            </view>
          </view>
          <view class="category-li">
-           <image src="../../static/ga005_40.png" mode="widthFix" style="width:30%"></image>
+           <view class="category-li-image">
+           	<image src="../../static/ga005_44.png" mode="aspectFit"></image>
+           </view>
            <view class="cate-li-right">
              <view class="li-r-top">
                <view class="li-r-top-left">
@@ -91,33 +140,102 @@
          </view>
        </view>
      </view>  
+		 </view>
 	</view>
 </template>
 
 <script>
+	import uniTag from '@/components/uni-tag/uni-tag.vue'
+	import uniIcon from '@/components/uni-icon/uni-icon.vue'
 	export default {
 		data() {
 			return {
 				
 			};
+		},
+		components: {
+			uniTag,
+			uniIcon
 		}
 	}
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
+	
+	/* 头部的样式*/
+	.headBox {
+	  position:absolute;
+		top:0;
+		right:0;
+		left:0;
+		.bgImage {
+			width: 100%;
+		}
+		.contentBox {
+			position:absolute;
+			left: 0;
+	    right: 0;
+			top: 100upx;
+	
+			.con_text {
+				padding: 0 30upx;
+				color: white;
+	      
+				.con_buttom {
+	        margin-top:20upx;
+	        display:flex;
+	        align-items:center;
+	        justify-content:space-between;
+					.conImg {
+	          background:white;
+	          width:500upx;
+	          height:50upx;
+	          padding-left:10upx;
+	          border-radius:10upx;
+	          image{
+	            position:relative;
+	            width:40upx;
+	            top:50%;
+	            transform:translateY(-50%);
+	          }
+					}
+				}
+			}
+		}
+	}
+	
+	
+ .tag-view{
+	 margin: 0 20upx 20upx 0 ;
+ }
+	h2{
+		font-weight:200;
+	}
+	
+	.text-sx-size{
+		font-size:16upx;
+		margin-left:10upx;
+	}
+	
+	.search-main{
+		  position:relative;
+			top:0;
+			right:0;
+			left:0;
+			bottom:0;
+			padding-top:230upx;
+	}
   
-  image{
-    width:120px;
-    height:80px;
-  }
-  
-  .historyBox{
+  .search-history{
+		background:#F7F5F4;
     box-sizing:border-box;
     padding:20upx 30upx;
-    .history-tab{
-      margin-top:16upx;
+    .history-items{
+      margin-top:20upx;
       min-height:200upx;
-      border:1px solid black;
+			display:flex;
+			flex-wrap:wrap;
+			overflow:hidden;
     }
   }
   
@@ -132,20 +250,31 @@
   .category-li{
       display:flex;
       background:white;
-      border:1px solid lightgray;
-      margin-bottom:16upx;
+			height:200upx;
+      margin-bottom:20upx;
       border-radius: 0 10upx 10upx 0;
+			.category-li-image{
+				box-sizing:border-box;
+				width:30%;
+				height:100%;
+				padding:10upx;
+				border-right:1upx solid #C1C1C1;
+			}
       .cate-li-right{
         flex:1;
         box-sizing:border-box;
-        padding:10upx 20upx;
-        border-left:1px solid lightgray;
+        padding:20upx 20upx;
         display:flex;
         flex-direction:column;
         justify-content:space-between;
         .li-r-top{
           display:flex;
           align-items:center;
+					.li-r-top-right{
+						display:flex;
+						align-items:center;
+						margin-left:20upx;
+					}
         }
       }
     }
